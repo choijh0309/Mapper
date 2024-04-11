b085083b3a0d51eb221bdeb8a72c6913b7d8f414
JUNHA CHOI <choijh0309@naver.com>
2024-04-11 18:00:01 +0900

    mapper commit

---
+++ src/main/java/com/codestates/coffee/mapper/CoffeeMapper.java
@@ -0,0 +1,17 @@
+package com.codestates.coffee.mapper;
+
+import com.codestates.coffee.dto.CoffeePatchDto;
+import com.codestates.coffee.dto.CoffeePostDto;
+import com.codestates.coffee.dto.CoffeeResponseDto;
+import com.codestates.coffee.entity.Coffee;
+import org.mapstruct.Mapper;
+import org.springframework.stereotype.Component;
+
+@Mapper(componentModel = "spring")
+public interface CoffeeMapper {
+    Coffee coffeePostDtoToCoffee(CoffeePostDto coffeePostDto);
+
+    Coffee coffeePatchDtoToCoffee(CoffeePatchDto coffeePatchDto);
+
+    CoffeeResponseDto coffeeToCoffeeResponseDto(Coffee coffee);
+}
